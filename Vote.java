92564b1a1dae6b222cbe254381f70f8f6a83734a
sahanayaligar12 <yaligarsahana5@gmail.com>
2025-04-15 10:06:03 +0530

    save

---
+++ Vote.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Vote {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter the age:");
+        int age = scanner.nextInt(); 
+
+        if (age >= 18) { 
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+        scanner.close();
+    }
+}
